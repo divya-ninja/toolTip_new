94c33ade36654c72a7b7880ba89910f1b6dc51c0
divya-ninja <divya140294@gmail.com>
2021-11-30 17:20:00 +0530

    final commit

--- src/Tip.js
+++ src/Tip.js
@@ -3,13 +3,13 @@ import './Tip.css';
 
 class Tip extends Component{
     render(){
+        // setting the position of tip container using the props.
         const setPosition = {
             marginLeft: this.props.left,
             marginTop: this.props.top
         }
         return(
             <div>
-                {/* <i class="pointer far fa-hand-pointer"></i> */}
                 <i class="pointer far fa-hand-point-down"></i>
                 <div className="tip" style={setPosition}>Hi! This is a sample tip.</div>
             </div>
